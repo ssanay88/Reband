cc1741d00692826ddc24d63562792154b3424ad8
yangsuhyuck <pplloo748@gmail.com>
2025-03-31 21:41:27 +0900

    밴드 등록 시 이미지 선택 기능 추가

--- app/src/main/java/com/project/reband/fragment/BandRegisterFragment.kt
+++ app/src/main/java/com/project/reband/fragment/BandRegisterFragment.kt
@@ -1,16 +1,20 @@
 package com.project.reband.fragment
 
-import android.app.Activity.RESULT_OK
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.project.reband.databinding.FragmentBandRegisterBinding
 import com.project.reband.viewmodel.BandRegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,26 +35,29 @@ class BandRegisterFragment : Fragment() {
     private var introduce: String = ""
     private var mediaUrl: String = ""
 
+    val pickImageOverSdk13 = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { imageUri ->
+        if (imageUri != null) {
+            setImageOnBandThumbnailBtn(imageUri)
+        } else {
+            Log.d("tngur", "[Image load fail over Sdk 13] - SDK : ${Build.VERSION.SDK_INT}")
+        }
+    }
+
+    val pickImageUnderSdk13 = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val imageUri: Uri? = result.data?.data
+            imageUri?.let {
+                setImageOnBandThumbnailBtn(imageUri)
+            }
+        } else {
+            Log.d("tngur", "[Image load fail under Sdk 13] - SDK : ${Build.VERSION.SDK_INT}")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    private val requestPermissionLauncher: ActivityResultLauncher<String> =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                // openGallery()
-            }
-        }
-
-    private val pickImageLauncher: ActivityResultLauncher<Intent> =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val data: Intent? = result.data
-                data?.data?.let {
-                    bandImage = it
-                }
-            }
-        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -75,6 +82,13 @@ class BandRegisterFragment : Fragment() {
         return binding.root
     }
 
+    private fun setImageOnBandThumbnailBtn(imageUri: Uri) {
+        Glide.with(this)
+            .load(imageUri)
+            .transform(CenterCrop())
+            .into(binding.btnBandThumbnail)
+    }
+
     private fun back() {
         requireActivity().supportFragmentManager.popBackStack()
     }
@@ -84,7 +98,16 @@ class BandRegisterFragment : Fragment() {
     }
 
     private fun getBandImage() {
-        // val targetPermisstion = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Android 13 이상
+            pickImageOverSdk13.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        } else {
+            // Android 13 이하
+            val intent = Intent(Intent.ACTION_PICK).apply {
+                type = "image/*"
+            }
+            pickImageUnderSdk13.launch(intent)
+        }
     }
 
 }
--- app/src/main/java/com/project/reband/viewmodel/BandRegisterViewModel.kt
+++ app/src/main/java/com/project/reband/viewmodel/BandRegisterViewModel.kt
@@ -1,13 +1,20 @@
 package com.project.reband.viewmodel
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.project.reband.data.band.BandCreateData
 import com.project.reband.network.band.BandRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class BandRegisterViewModel: ViewModel() {
-    private val repository = BandRepository()
+@HiltViewModel
+class BandRegisterViewModel @Inject constructor(
+    private val repository: BandRepository
+): ViewModel() {
 
     fun createBand(bandCreateData: BandCreateData) {
         viewModelScope.launch {
